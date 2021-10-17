72be70d2d84b3de44c24f331b0a9c5967f93f152
Todor Trundev <trundev@gmail.com>
2021-10-17 16:07:39 +0300

    Uneven signal intervals support
    Use mean derivatives to fix the approximation errors, when 'time'
    advances irregularly.
    
    Detect approximation failures, like zero interval.

--- extrapolator.py
+++ extrapolator.py
@@ -1,13 +1,19 @@
 class extrapolator:
     """Extrapolate signal value, by keeping track of its derivatives over time"""
-    # Vector of the singal value deltas (val, time):
+    # Vector of the signal value deltas (val, time):
     # 0 - values itself (zero-derivative):
     #       delta0_next: value
     #       time0_next: time
-    # 1 - first delta:
-    #       delta1_next: (delta0_next - delta0_last) / (time0_next - time0_last)
-    #       time1_next: (time0_next + time0_last) / 2
-    # 2 - etc.
+    # 1 - first mean derivative for the interval 'time0_last' to 'time0_next':
+    #       delta1_next: 1 * (delta0_next - delta0_last) / (time0_next - time0_last)
+    #       time1_next: time0_last
+    # 2 - second mean derivative for the interval 'time1_last' to 'time0_next'):
+    #       delta2_next: 2 * (delta1_next - delta1_last) / (time0_next - time1_last)
+    #       time2_next: time1_last
+    # 3 - etc.
+    #
+    # Note that, all mean derivative intervals end at 'time0_next' and each one is
+    # included in the next
     deltas = []
 
     def __init__(self, src=None):
@@ -15,49 +21,45 @@ class extrapolator:
         if src is not None:
             self.deltas = src.deltas.copy()
 
-    def __next_times(self, time, delta_rank=0, new_len=None):
+    def __next_times(self, time, delta_rank=0):
         """Select new time vector, based on a specific derivative"""
-        if new_len is None:
-            new_len = len(self.deltas)
-        next_times = [None] * new_len
-        next_times[delta_rank] = time
+        # Update toward intergrals, when delta_rank > 0
+        next_times = [time] * (delta_rank + 1)
+        assert delta_rank == 0, 'TODO: Use non-zero deltas toward integrals'
 
         # Update toward differentials
-        for i in range(delta_rank, new_len - 1):
-            next_times[i + 1] = (next_times[i] + self.deltas[i][1]) / 2
-
-        # Update toward intergrals, when delta_rank > 0
-        for i in reversed(range(0, delta_rank)):
-            next_times[i] = 2 * next_times[i + 1] - self.deltas[i][1]
-
+        next_times += [v[1] for v in self.deltas[delta_rank:]]
         return next_times
 
     def update(self, val, time, delta_rank=0):
         """Update 'deltas' based on a derivative value at specific moment"""
-        # Ensure there is a slot for the 'delta_rank' and an extra one after the extiting
-        next_deltas = [None] * (1 + max(len(self.deltas), delta_rank))
-        next_deltas[delta_rank] = val
-
         # Advance delta's times, based on the required time0
-        next_times = self.__next_times(time, delta_rank, len(next_deltas))
+        next_times = self.__next_times(time, delta_rank)
+
+        # Initial next-delta array
+        next_deltas = [None] * len(next_times)
+        next_deltas[delta_rank] = val
 
         # Update toward differentials
         for i in range(delta_rank, len(self.deltas)):
             delta_v = next_deltas[i] - self.deltas[i][0]
-            delta_t = next_times[i] - self.deltas[i][1]
-            next_deltas[i + 1] = delta_v / delta_t
+            delta_t = time - self.deltas[i][1]
+            if delta_t == 0:
+                break   # Leave None in 'next_deltas'
+            next_deltas[i + 1] = (i + 1 - delta_rank) * delta_v / delta_t
         # Drop the extra delta-rank, if zero
-        if delta_rank < len(next_deltas) - 1 and next_deltas[-1] == 0.:
+        if delta_rank < len(next_deltas) - 1 and not next_deltas[-1]:
             next_deltas = next_deltas[:-1]
 
         # Update toward intergrals, when delta_rank > 0
         for i in reversed(range(0, delta_rank)):
-            delta_t = next_times[i] - self.deltas[i][1]
-            next_deltas[i] = self.deltas[i][0] + next_deltas[i + 1] * delta_t
+            delta_t = next_times[0] - self.deltas[i][1]
+            delta_v = next_deltas[i + 1] * delta_t / (i + 1)
+            next_deltas[i] = self.deltas[i][0] + delta_v * delta_t
 
         # Make the new vector actual one
         self.deltas = list(zip(next_deltas, next_times))
-        return True
+        return all(d is not None for d in next_deltas)
 
     def extrapolate(self, time0):
         """Generate extrapolated 'deltas' object at specific moment"""
@@ -65,13 +67,13 @@ class extrapolator:
             return False
 
         # Advance delta's times, based on the required time0
-        next_times = self.__next_times(time0)
+        next_times = self.__next_times(time0)[:len(self.deltas)]
 
         # Extrapolate deltas at selected moments, assuming the last delta is constant
         self.deltas[-1] = (self.deltas[-1][0], next_times[-1])
         for i in reversed(range(0, len(self.deltas) - 1)):
-            delta_t = next_times[i] - self.deltas[i][1]
-            delta_v = self.deltas[i + 1][0] * delta_t
+            delta_t = time0 - self.deltas[i][1]
+            delta_v = self.deltas[i + 1][0] * delta_t / (i + 1)
             self.deltas[i] = (self.deltas[i][0] + delta_v, next_times[i])
         return True
 
